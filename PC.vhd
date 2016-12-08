bc3e18994722226f80e6982f85e6e9546c87a76c
EricSong8612 <js8612@nyu.edu>
2016-12-07 20:09:54 -0500

    Add files via upload

---
+++ PC.vhd
@@ -0,0 +1,57 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:57:29 12/06/2016 
+-- Design Name: 
+-- Module Name:    PC - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity PC is
+
+Port ( clk : in  STD_LOGIC;
+       reset : in  STD_LOGIC;
+		 input : in  STD_LOGIC_VECTOR (31 downto 0);
+       output : out  STD_LOGIC_VECTOR (31 downto 0));
+
+end PC;
+
+architecture Behavioral of PC is
+
+begin
+
+process(reset,clk)
+  begin
+    if reset='1' then
+       output <= X"00000000";
+    elsif rising_edge(clk) then
+       output<=input;
+    end	if;  
+end process;
+
+end Behavioral;
+
---
+++ shift_left_2.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:18:04 12/07/2016 
+-- Design Name: 
+-- Module Name:    shift_left_2 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Shift_Left_2 is
+    Port ( input : in  STD_LOGIC_VECTOR (31 downto 0);           
+			  output : out STD_LOGIC_VECTOR(31 downto 0));
+			  
+end Shift_Left_2;
+
+architecture Behavioral of Shift_Left_2 is
+
+begin
+
+		output <=  std_logic_vector(unsigned(input) sll 2);
+
+end Behavioral;
+
